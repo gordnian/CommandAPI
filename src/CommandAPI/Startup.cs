bc30c2010b92ef1141f102ec1210e8d796267e10
Keyvan Gordnian <kgordnian@wencomine.com>
2021-11-02 19:56:00 -0700

    Added Model and Mock Repository

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -17,6 +13,8 @@ namespace CommandAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
